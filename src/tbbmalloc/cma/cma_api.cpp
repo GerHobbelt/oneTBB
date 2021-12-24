38db20a38e7a6f2b6b4637233f6b1d441f0217db
GoldJohnKing <GoldJohnKing@Live.cn>
2021-12-24 20:04:06 +0800

    Remove interlocked CmaMappedMemory counter as Arma 3 does not require it

--- src/tbbmalloc/cma/cma_api.cpp
+++ src/tbbmalloc/cma/cma_api.cpp
@@ -7,16 +7,14 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-LONG64 CmaMappedMemory = 0;
-
 size_t __stdcall MemTotalCommitted(void)
 {
-	return CmaMappedMemory;
+	return 0;
 }
 
 size_t __stdcall MemTotalReserved(void)
 {
-	return CmaMappedMemory;
+	return 0;
 }
 
 size_t __stdcall MemFlushCache(size_t /*size*/)
@@ -42,44 +40,22 @@ size_t __stdcall MemSizeA(void* mem, size_t /*align*/)
 void* __stdcall MemAlloc(size_t size)
 {
 	void* mem = scalable_malloc(size);
-	if (!mem)
-		return mem;
-
-	MEMORY_BASIC_INFORMATION mbi;
-	if (VirtualQuery(mem, &mbi, sizeof(mbi)) == sizeof(mbi))
-		InterlockedAdd64(&CmaMappedMemory, mbi.RegionSize);
-
 	return mem;
 }
 
 void* __stdcall MemAllocA(size_t size, size_t align)
 {
 	void* mem = scalable_aligned_malloc(size, align);
-	if (!mem)
-		return mem;
-
-	MEMORY_BASIC_INFORMATION mbi;
-	if (VirtualQuery(mem, &mbi, sizeof(mbi)) == sizeof(mbi))
-		InterlockedAdd64(&CmaMappedMemory, mbi.RegionSize);
-
 	return mem;
 }
 
 void __stdcall MemFree(void* mem)
 {
-	MEMORY_BASIC_INFORMATION mbi;
-	if (VirtualQuery(mem, &mbi, sizeof(mbi)) == sizeof(mbi))
-		InterlockedAdd64(&CmaMappedMemory, -(LONG64)mbi.RegionSize);
-
 	scalable_free(mem);
 }
 
 void __stdcall MemFreeA(void* mem)
 {
-	MEMORY_BASIC_INFORMATION mbi;
-	if (VirtualQuery(mem, &mbi, sizeof(mbi)) == sizeof(mbi))
-		InterlockedAdd64(&CmaMappedMemory, -(LONG64)mbi.RegionSize);
-
 	scalable_aligned_free(mem);
 }
 
